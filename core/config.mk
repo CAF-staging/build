8779cdd4f51e053949b5bec7b0d8aa98356c9d86
kar <kar2wlan@gmail.com>
2016-10-11 08:55:35 +0530

    stupid linux...derp fixed path

--- core/config.mk
+++ core/config.mk
@@ -917,7 +917,7 @@ endif
 ifneq ($(CAF_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/CAF/sepolicy/sepolicy.mk)
+$(eval include vendor/caf/sepolicy/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
